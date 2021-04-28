0f094bc9b41646c2c9491cfd006a5916b7fbb5d2
GK <jpkutner+github@gmail.com>
2021-04-27 21:01:47 -0500

    Co-authored-by: HipsterDave <HipsterDave@users.noreply.github.com>

--- car.py
+++ car.py
@@ -27,7 +27,20 @@ def clear():
 		os.system("clear")
 
 class car():
-    def __init__(self):
-        self.speed
-        self.off_road
-        
+    def __init__(self, speed, off_road, fuel, safety, luxury, size):
+        self.speed = speed
+        self.off_road = off_road
+        self.fuel = fuel
+		self.safety = safety
+		self.luxury = luxury
+		self.size = size
+
+bmw_isetta = car(2, 0, 1, 2, 1, 1)
+delorean_dmc12 = car(4, 3, 5, 3, 6, 4)
+jeep_wrangler = car(5, 7, 3, 4, 4, 6)
+toyota_camry = car(5, 4, 7, 6, 4, 5)
+m1_abrams = car(3, 10, 3, 7, 3, 10)
+bigfoot_5 = car(3, 9, 2, 5, 3, 10)
+toy_car = car(2, 1, 3, 1, 0, 1)
+amphicar_model770 = car(3, 10, 4, 2, 4, 4)
+fiat_multipla
